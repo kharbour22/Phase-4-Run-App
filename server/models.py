79e0289997f2f5af4f62d5cce9c790a76deb26e2
KevinHarbour <kharbour22@gmail.com>
2024-04-15 16:21:30 -0400

    added seed.py

--- server/models.py
+++ server/models.py
@@ -4,3 +4,12 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Run(db.Model, SerializerMixin):
+    __tablename__ = 'runs'
+
+    id = db.Column(db.Integer, primary_key = True)
+    location = db.Column(db.String, nullable = False)
+    image = db.Column(db.String, nullable = False)
+    link = db.Column(db.String, nullable = False)
+
+
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,22 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Run
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        Run.query.delete()
         print("Starting seed...")
-        # Seed code goes here!
+
+        run1 = Run(location="North Table Mountain Park", image='c:\\Users\\kharb\\Pictures\\Screenshots\\NTM.png', link='https://www.alltrails.com/trail/us/colorado/north-table-mountain-west-loop')
+        run2 = Run(location= "Big Dry Creek", image= 'c:\\Users\kharb\Pictures\\Screenshots\\BDC.png', link = 'https://www.alltrails.com/trail/us/colorado/big-dry-creek-2-mi-north' )
+
+        db.session.add(run1)
+        db.session.add(run2)
+        db.session.commit()
+
+
+
+        
+        print("Completed seeding!")
