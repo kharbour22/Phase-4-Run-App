a1c03896776c19fca22300d3a81ba4c61237a04f
KevinHarbour <kharbour22@gmail.com>
2024-04-15 16:46:25 -0400

    Created Route

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>
+    }
+])
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router = {router}  />);
--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -12,10 +12,17 @@ from config import app, db, api
 from models import Run
 
 # Views go here!
+class AllRuns(Resource):
+
+    def get(self):
+        response_body = [run.to_dict(only = ('id', 'location', 'image', 'link')) for run in Run.quesry.all()]
+        return make_response(response_body, 200)
+api.add_resource(AllRuns, '/runs')
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
 
 
 if __name__ == '__main__':
